2364df50d1a5bd10a547f83efda63dd276a8b247
Adjagarail <adjagarail@gmail.com>
2020-01-15 12:31:46 +0100

    database and table created

---
+++ src/Entity/Articles.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ArticlesRepository")
+ */
+class Articles
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Titre;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Postere;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $Article;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $DatePublication;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Categories", inversedBy="articles")
+     */
+    private $Categorie;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->Titre;
+    }
+
+    public function setTitre(string $Titre): self
+    {
+        $this->Titre = $Titre;
+
+        return $this;
+    }
+
+    public function getPostere(): ?string
+    {
+        return $this->Postere;
+    }
+
+    public function setPostere(string $Postere): self
+    {
+        $this->Postere = $Postere;
+
+        return $this;
+    }
+
+    public function getArticle(): ?string
+    {
+        return $this->Article;
+    }
+
+    public function setArticle(string $Article): self
+    {
+        $this->Article = $Article;
+
+        return $this;
+    }
+
+    public function getDatePublication(): ?\DateTimeInterface
+    {
+        return $this->DatePublication;
+    }
+
+    public function setDatePublication(\DateTimeInterface $DatePublication): self
+    {
+        $this->DatePublication = $DatePublication;
+
+        return $this;
+    }
+
+    public function getCategorie(): ?Categories
+    {
+        return $this->Categorie;
+    }
+
+    public function setCategorie(?Categories $Categorie): self
+    {
+        $this->Categorie = $Categorie;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Categories.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\CategoriesRepository")
+ */
+class Categories
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Nom;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Descriptif;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Articles", mappedBy="Categorie")
+     */
+    private $articles;
+
+    public function __construct()
+    {
+        $this->articles = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->Nom;
+    }
+
+    public function setNom(string $Nom): self
+    {
+        $this->Nom = $Nom;
+
+        return $this;
+    }
+
+    public function getDescriptif(): ?string
+    {
+        return $this->Descriptif;
+    }
+
+    public function setDescriptif(string $Descriptif): self
+    {
+        $this->Descriptif = $Descriptif;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Articles[]
+     */
+    public function getArticles(): Collection
+    {
+        return $this->articles;
+    }
+
+    public function addArticle(Articles $article): self
+    {
+        if (!$this->articles->contains($article)) {
+            $this->articles[] = $article;
+            $article->setCategorie($this);
+        }
+
+        return $this;
+    }
+
+    public function removeArticle(Articles $article): self
+    {
+        if ($this->articles->contains($article)) {
+            $this->articles->removeElement($article);
+            // set the owning side to null (unless already changed)
+            if ($article->getCategorie() === $this) {
+                $article->setCategorie(null);
+            }
+        }
+
+        return $this;
+    }
+}
